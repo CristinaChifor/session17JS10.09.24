d6dcae8f256878f271baa8d47256d0a3b8496544
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-09-15 21:10:46 +0300

    Work in progress

--- script.js
+++ script.js
@@ -45,6 +45,10 @@ document.addEventListener('DOMContentLoaded', async () => {
                             <div class="description">${product.description}</div>
                             </div>`;
                      listContainer.appendChild(productContainer);
+                     productContainer.addEventListener("click", () => {
+                        localStorage.setItem("productId", product.id);
+                        window.location.href = "./product_page.html";
+                     });
     }
     });
 
